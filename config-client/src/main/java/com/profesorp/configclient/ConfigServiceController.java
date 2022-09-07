659511c12922066a36c1cc56c4d93dc46179e0bd
Seedarla Dasu <Seedarla.Dasu@coforge.com>
2022-09-07 19:41:11 +0530

    Updates

--- config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
+++ config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
@@ -2,11 +2,13 @@ package com.profesorp.configclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.profesorp.configclient.bean.BeanConfiguration;
 
+@RefreshScope
 @RestController
 public class ConfigServiceController {
 	
